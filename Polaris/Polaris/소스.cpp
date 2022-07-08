7cadd5f4ddbd2db29f7d723bff22232e030e992d
Polaris5124 <107294836+Polaris5124@users.noreply.github.com>
2022-07-08 13:49:47 +0900

    Update 소스.cpp

--- Polaris/Polaris/소스.cpp
+++ Polaris/Polaris/소스.cpp
@@ -16,37 +16,6 @@ int main()
 		result = a + b;
 		printf("%f + %f = %f", a, b, result);
 	}
-	if (k == '-')
-	{
-		result = a - b;
-		printf("%f - %f = %f", a, b, result);
-	}
-	if (k == '*')
-	{
-		result = a * b;
-		printf("%f * %f = %f", a, b, result);
-	}
-	if (k == '/') {
-		if (b == 0)
-		{
-			//0으로 나눌때 오류 방지#include<stdio.h>
-int main()
-
-{
-	float a, b;
-	float result;
-	char k;
-	printf("첫번째 값을 입력해주세요:");
-	scanf_s("%f", &a);
-	printf("연산 기호를 입력해주세요:");
-	scanf_s("%*c%c", &k, 1);
-	printf("두번째 값을 입력해주세요:");
-	scanf_s("%f", &b);
-	if (k == '+')
-	{
-		result = a + b;
-		printf("%f + %f = %f", a, b, result);
-	}
 	else if (k == '-')
 	{
 		result = a - b;
